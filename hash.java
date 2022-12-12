53f5269e4749bfe4dd7c6e90fe6d006862e884b5
Dineshbabu <116623687+dineshbabu2901@users.noreply.github.com>
2022-12-12 11:31:28 +0530

    Add files via upload

---
+++ hash.java
@@ -0,0 +1,16 @@
+package javaPrograms;
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetDemo {
+	public static void main(String[] args) {
+		LinkedHashSet <Integer> lhs=new LinkedHashSet<>();
+		lhs.add(1);
+		lhs.add(2);
+		lhs.add(3);
+		lhs.add(4);
+		lhs.add(5);
+		System.out.println(lhs);
+	}
+
+}
